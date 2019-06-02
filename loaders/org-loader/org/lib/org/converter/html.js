3025f2292cf14079055746756813c23c5ee095a3
zy <oldzhong@qq.com>
2019-06-02 17:49:42 +0800

    nil

--- loaders/org-loader/org/lib/org/converter/html.js
+++ loaders/org-loader/org/lib/org/converter/html.js
@@ -242,8 +242,8 @@ ConverterHTML.prototype = {
       var imgText = this.getNodeTextContent(node);
       var imgSrc = node.src
       if (imgSrc.indexOf('../static/') == 0) {
-        // imgSrc = imgSrc.replace('../static/', '/static/')
-        imgSrc = imgSrc.replace('../static/', 'https://msv4.cdn.bcebos.com/static/')
+        imgSrc = imgSrc.replace('../static/', '/static/')
+        // imgSrc = imgSrc.replace('../static/', 'https://msv4.cdn.bcebos.com/static/')
       }
       fields = imgSrc.split('/')
       fields.splice(fields.length - 1, 0, 'small')
